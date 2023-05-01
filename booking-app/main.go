822a7b589b3bde3bf3f31b61ba02e98e1fc72acd
Abdul Majid <76196721+majid-2002@users.noreply.github.com>
2023-05-01 14:03:45 +0530

    Added the firstName slice to show only firstNames in the print

--- booking-app/main.go
+++ booking-app/main.go
@@ -1,49 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	var conferenceName string = "Go conference"
 	const conferenceTickets int = 50
-	remainingTickets := 50 // syntatic sugar
+	remainingTickets := 50    // syntatic sugar
+	var bookings = []string{} //? this is a slice. A slice is just like an array but like a dynamic size array
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Println("We have total of", conferenceTickets, "tickets and the remaining tickets are", remainingTickets)
 	fmt.Println("Get your tickets to attend..")
 
-	var bookings = []string{} //? this is a slice. A slice is just like an array but like a dynamic size array
-	var firstName string
-	var lastName string
-	var userTickets int
-	var userEmail string
+	for {
 
-	fmt.Println("Enter your first name")
-	fmt.Scan(&firstName)
+		var firstName string
+		var lastName string
+		var userTickets int
+		var userEmail string
 
-	fmt.Println("Enter your last name")
-	fmt.Scan(&lastName)
+		fmt.Println("Enter your first name")
+		fmt.Scan(&firstName)
 
-	fmt.Println("Enter your email")
-	fmt.Scan(&userEmail)
+		fmt.Println("Enter your last name")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Enter the number of tickets you want to book")
-	fmt.Scan(&userTickets)
+		fmt.Println("Enter your email")
+		fmt.Scan(&userEmail)
 
-	var userName string = firstName + " " + lastName
+		fmt.Println("Enter the number of tickets you want to book")
+		fmt.Scan(&userTickets)
 
-	remainingTickets -= userTickets
+		var userName string = firstName + " " + lastName
 
-	//? this is how we add a new item to the slice by using the append method in go lang
-	bookings = append(bookings, userName)
+		remainingTickets -= userTickets
 
-	// fmt.Printf("the whole slice : %v\n", bookings)
-	// fmt.Printf("the first valu : %v\n", bookings[0])
-	// fmt.Printf("Slice type : %T\n", bookings)
-	// fmt.Printf("Slice length : %v\n", len(bookings))
+		//? this is how we add a new item to the slice by using the append method in go lang
+		bookings = append(bookings, userName)
 
-	fmt.Printf("Thank you %v for booking %v tickets. You will recieve a confirmation email at %v.\n", userName, userTickets, userEmail)
-	fmt.Println("Remaining tickets are", remainingTickets)
+		// fmt.Printf("the whole slice : %v\n", bookings)
+		// fmt.Printf("the first valu : %v\n", bookings[0])
+		// fmt.Printf("Slice type : %T\n", bookings)
+		// fmt.Printf("Slice length : %v\n", len(bookings))
 
-	fmt.Println("These are all the bookings", bookings)
+		fmt.Printf("Thank you %v for booking %v tickets. You will recieve a confirmation email at %v\n", userName, userTickets, userEmail)
+		fmt.Println("Remaining tickets are", remainingTickets)
+
+		var firstNames = []string{}
+
+		for _, value := range bookings {
+			var names = strings.Fields(value)
+			firstNames = append(firstNames, names[0])
+		}
+
+		fmt.Println("These are all the bookings", firstNames)
+	}
 
 }
